626517a53153389f27b25d857523d15a35206046
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2026-08-02 12:20:00 +0300

    Add files via upload

---
+++ Problem #1/Problem #/Problem #.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printYourName(string name)
+{
+    cout << "your name : " << name << endl;
+}
+
+
+int main()
+{
+    printYourName("Dracula Alhorany");
+    
+    return (0);
+}
