8d784238fabe5691529e411fd9e79e4177abcc5e
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2026-08-06 19:39:27 +0300

    Add files via upload

---
+++ Problem #9/Problem #/Problem #.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int  ReadNumber(int &number)
+{
+
+	cout << "Enter Number: " << endl;
+	cin >> number;
+	return number;
+}
+
+void ReadNumbers(int& num1, int& num2, int& num3)
+{
+	ReadNumber(num1);
+	ReadNumber(num2);
+	ReadNumber(num3);
+}
+
+//void  ReadMultipleNumber (int Numbers[3])
+//{
+//
+//	for (int index = 0; index < 3; index++)
+//	{
+//		cout << "Enter Number " << index+1 << " :" << endl;
+//		ReadNumber(Numbers[index]);
+//	}
+//}
+
+
+//int summation(int numbers[3])
+//{
+//	int sum = 0;
+//	for (int i = 0; i < 3; i++)
+//		sum += numbers[i];	
+//	return sum;
+//}
+
+int sumOfThree(int num1, int num2, int num3)
+{
+	return num1 + num2 + num3;
+}
+
+
+void printResult(int summation)
+{
+		cout << "the summation is: " << summation << endl;
+}
+
+
+int main()
+{
+	int num1 , num2 , num3;
+	ReadNumbers(num1 , num2 , num3);
+	printResult(sumOfThree(num1 ,num2 , num3));
+	return (0);
+}
+
+
+//
+//int main()
+//{
+//	int Numbers[3];
+//	ReadMultipleNumber(Numbers);
+//	printResult(summation(Numbers));
+//    return (0);
+//}
