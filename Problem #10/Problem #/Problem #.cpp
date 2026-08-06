ce84d93b6ab8093403c3c0f0a9885e2e05a93c2d
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2026-08-06 19:59:48 +0300

    Add files via upload

---
+++ Problem #10/Problem #/Problem #.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int  ReadMark(int &Mark , int index)
+{
+
+	cout << "Enter Mark: " << endl;
+	cin >> Mark;
+	return Mark;
+}
+
+void ReadMultipleMarks(int& num1, int& num2, int& num3)
+{
+	ReadMark(num1 , 1);
+	ReadMark(num2 , 2);
+	ReadMark(num3 , 3);
+}
+
+int sumOfThree(int num1, int num2, int num3)
+{
+	return num1 + num2 + num3;
+}
+
+
+
+float clculateAVG(int mark1 , int mark2 , int mark3)
+{
+	return (sumOfThree(mark1 , mark2 , mark3) / 3.0);
+}
+
+
+
+
+void printResult(int avg)
+{
+		cout << "the avg is: " << avg << endl;
+}
+
+int main()
+{
+	int mark1, mark2, mark3;
+	ReadMultipleMarks(mark1 ,mark2 , mark3);
+	printResult(clculateAVG(mark1 , mark2 , mark3));
+
+}
+
+
+
+
+
+
+
